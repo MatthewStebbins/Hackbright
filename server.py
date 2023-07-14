ecfd9b047b734e14a85057e974c3e97d41d52fcf
Matthew Stebbins <mattcatt89@yahoo.com>
2023-07-14 18:43:01 -0500

    first commit

---
+++ server.py
@@ -0,0 +1,26 @@
+import requests
+
+from random import choice, randint
+from flask import Flask, render_template, request
+
+URL = "https://api.jikan.moe/v4/characters?page="
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def api_call():
+    randnum = randint(1,1000)
+    res = requests.get(URL+ str(randnum))
+    results = res.json()
+    image = results['data'][2]['images']['jpg']['image_url']
+    name = results['data'][2]['name']
+    return render_template('main.html', image=image, name=name)
+
+@app.route("/rules")
+def rules():
+    return render_template('rules.html')
+
+if __name__ == '__main__':
+
+    app.run(debug=True, host="0.0.0.0")
